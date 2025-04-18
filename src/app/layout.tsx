d4252ddbc0d955aff04cb419a0f726dad0d238a3
ramseywiz <ramchandoklow@gmail.com>
2025-04-18 13:44:40 -0500

    removed tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Ramsey's Portfolio!",
@@ -25,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={outfit.className}>
         {children}
       </body>
     </html>
