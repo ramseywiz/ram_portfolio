1dc442c1517e874660ea030bf65126a90cf05cd6
ramseywiz <ramchandoklow@gmail.com>
2025-04-18 14:34:29 -0500

    removed lots of weird stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
-import "./globals.css";
+import "../../styles/globals.css";
 
 
 const outfit = Outfit({subsets: ["latin"]});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello Neighbor!</h1>
+    <>
+      <h1>Hello Neighbor!</h1>
+    </>
   );
 }
